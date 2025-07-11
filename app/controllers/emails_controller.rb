e768eae31d956443ccb5c4d6dd577e7a9aea4ebb
Daz <djones@yozu.co.uk>
2025-07-11 19:17:08 +0100

    smushed without stimulus

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,6 +3,13 @@ class EmailsController < ApplicationController
   end
 
   def show
-    @email = Email.find params[:id]
+    @email = Email.find(params[:id])
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.update("emails", target: "emails", partial: "emails/list", locals: { selected: @email }, **{ method: "morph" })
+      end
+      format.html # fallback for normal requests
+    end
   end
 end
