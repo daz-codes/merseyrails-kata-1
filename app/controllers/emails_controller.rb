8e86c60d5220ff868fdfe11c64242f042a558775
Daz <djones@yozu.co.uk>
2025-07-11 19:50:09 +0100

    kind of working without stimulus

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,15 +1,19 @@
 class EmailsController < ApplicationController
-  def index
-  end
-
   def show
     @email = Email.find(params[:id])
 
+    # This just sends a turbo-stream over ActionCable
+    Turbo::StreamsChannel.broadcast_replace_to(
+      "emails",                         # stream name (e.g., turbo_stream_from "emails" on the client)
+      target: "emails",                 # DOM ID to replace
+      partial: "emails/list",           # partial to render
+      locals: { selected: @email }
+    )
+
+    # Continue with regular response if needed
     respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.update("emails", target: "emails", partial: "emails/list", method: "morph", locals: { selected: @email })
-      end
-      format.html # fallback for normal requests
+      format.turbo_stream
+      format.html
     end
   end
 end
