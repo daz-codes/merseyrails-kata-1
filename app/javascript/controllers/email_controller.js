2bf72684e55d989375c3730d224b29ce3a68ade3
Daz <djones@yozu.co.uk>
2025-07-14 08:13:32 +0100

    highlight current when page loaded from url

--- app/javascript/controllers/email_controller.js
+++ app/javascript/controllers/email_controller.js
@@ -2,11 +2,23 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
+    const id = extractEmailIdFromUrl(window.location.pathname);
+    const email = document.getElementById(`email_${id}`);
+    if (email) highlightCurrent(email);
     this.element.addEventListener("click", (event) => {
       const old_email = this.element.querySelector(".bg-blue-100");
       old_email?.classList?.remove("bg-blue-100");
       const email = event.target.closest("a");
-      email.classList.add("bg-blue-100");
+      highlightCurrent(email);
     });
   }
+}
+
+function extractEmailIdFromUrl(path) {
+  const match = path.match(/^\/emails\/(\d+)/);
+  return match ? match[1] : null;
+}
+
+function highlightCurrent(email) {
+  email.classList.add("bg-blue-100");
 }
