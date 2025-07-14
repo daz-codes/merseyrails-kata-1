28081a58efb64d787a62c911609b3455ededa45e
Daz <djones@yozu.co.uk>
2025-07-14 12:02:43 +0100

    Removed turbo morph helper method, updated stimulus controller

--- app/javascript/controllers/email_controller.js
+++ app/javascript/controllers/email_controller.js
@@ -1,24 +1,14 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static classes = ["active"];
+
   connect() {
-    const id = extractEmailIdFromUrl(window.location.pathname);
-    const email = document.getElementById(`email_${id}`);
-    if (email) highlightCurrent(email);
     this.element.addEventListener("click", (event) => {
-      const old_email = this.element.querySelector(".bg-blue-100");
-      old_email?.classList?.remove("bg-blue-100");
+      const prevEmail = this.element.querySelector(`.${this.activeClass}`);
+      prevEmail?.classList?.remove(this.activeClass);
       const email = event.target.closest("a");
-      highlightCurrent(email);
+      email.classList.add(this.activeClass);
     });
   }
-}
-
-function extractEmailIdFromUrl(path) {
-  const match = path.match(/^\/emails\/(\d+)/);
-  return match ? match[1] : null;
-}
-
-function highlightCurrent(email) {
-  email.classList.add("bg-blue-100");
 }
