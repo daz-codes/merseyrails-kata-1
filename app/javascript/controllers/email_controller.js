dc8e553f475c630aa9ad8a508b2e12d97c54662f
Daz <djones@yozu.co.uk>
2025-07-19 10:28:08 +0100

    using my own version of stimulus

--- app/javascript/controllers/email_controller.js
+++ app/javascript/controllers/email_controller.js
@@ -1,14 +1,21 @@
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from "@daz/stimulus";
 
 export default class extends Controller {
   static classes = ["active"];
+  static values = { active: String };
 
   connect() {
+    const email = this.findElement(this.activeValue);
+    if (email) {
+      email.scrollIntoView();
+    }
+
     this.element.addEventListener("click", (event) => {
-      const prevEmail = this.element.querySelector(`.${this.activeClass}`);
+      const prevEmail = this.findElement(this.activeValue);
       prevEmail?.classList?.remove(this.activeClass);
       const email = event.target.closest("a");
       email.classList.add(this.activeClass);
+      this.activeValue = email.id;
     });
   }
 }
